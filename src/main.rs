0090c9d5a548461e5968977917f019d9077da42a
CheemsBread <dogecheemsbread@gmail.com>
2023-05-24 21:42:48 -0400

    Username, Date, Time
    I added most of the code I wanted to add. It prints the user's name, The Date, and the Time.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use chrono::prelude::*;
+use users::get_current_username;
 
 fn main() {
+    //get's the user name of the user and then prints
+    match get_current_username() {
+        Some(username) => {
+            println!("Hello: {}", username.to_string_lossy());
+        }
+        None => {
+            eprintln!("Failed to retrieve the username.");
+        }
+    }
+
+    //get's time and then prints it
     let local: DateTime<Local> = Local::now();
-    println!("Current time: {}", local.format("%Y-%m-%d %I:%M:%S %p"));
+    //date
+    println!("The date is: {}", local.format("%Y-%m-%d"));
+    //time
+    println!("The time is: {}", local.format("%I:%M:%S %p"));
 }
